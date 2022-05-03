a3cef9fa3c0ec0a2efa9729e29c778e3d51b6730
Jonas <Joonaskaiser@gmail.com>
2022-05-03 21:03:55 +0200

    Initial Commit

---
+++ CaskStore_App/CaskStore_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaskStore_AppApp.swift
+//  CaskStore_App
+//
+//  Created by Jonas Kaiser on 03.05.22.
+//
+
+import SwiftUI
+
+@main
+struct CaskStore_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
